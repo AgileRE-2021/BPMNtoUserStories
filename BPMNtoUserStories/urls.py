3496840375ecde488c3fca323c7ce098f1c3e8af
Kinaraalg <Kinaraalghifari17@gmail.com>
2021-05-16 14:43:52 +0700

    first commit

---
+++ BPMNtoUserStories/urls.py
@@ -0,0 +1,15 @@
+from django.contrib import admin
+from django.urls import path
+from . import views
+from django.conf import settings
+from django.conf.urls.static import static
+
+
+urlpatterns = [
+    # path('admin/', admin.site.urls),
+    path('', views.home, name='home'),
+    path('uploadfile/', views.uploadpage, name='uploadpage'),
+    path('proses/', views.parsing, name='parsing'),
+]
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
---
+++ BPMNtoUserStories/views.py
@@ -0,0 +1,94 @@
+from django.shortcuts import render
+from django.core.files.storage import FileSystemStorage
+import xml.etree.ElementTree as ET
+import os
+
+
+def masterpages(request):
+     return render(request,'masterpage.html')
+
+def home(request):
+     return render(request,'home.html')
+     
+def uploadpage(request):
+     return render(request,'uploadfile.html')
+
+def parsing(request):
+    if request.method == 'POST':
+        uploaded_file = request.FILES['xml']
+        fs = FileSystemStorage()
+        fs.save(uploaded_file.name, uploaded_file)
+        dir = ('.\media')
+        file_name = str(uploaded_file.name)
+        full_path = os.path.join(dir,file_name)
+        file_output = "clean" +file_name
+        full_path_new = os.path.join(dir,file_output)
+        with open(full_path, 'r') as infile, open(full_path_new, "w") as outfile:
+            data = infile.read()
+            data = data.replace('xmlns="http://www.wfmc.org/2009/XPDL2.2"', '')
+            outfile.write(data)
+
+        context = {}
+        with open(full_path_new, 'r') as filexml :
+            tree = ET.parse(filexml)
+            root = tree.getroot()
+            list_actor = []
+            list_activity = []
+            for i in tree.findall('.//Lane'):
+                name = i.get('Name')
+                list_actor.append(name)
+            for i in tree.findall('.//Activity'):
+                activity = i.get('Name')
+                if (activity != "Start" and activity != "End" and activity != " "):
+                    list_activity.append(activity)
+            total_actor = len(list_actor)
+            
+            
+
+            # mengecek apakah total aktor lebih dari satu dan mengambil nilai koordinat dari setiap lane didalam array
+            if (total_actor > 1): 
+                Lane_Coordinates = [] #koordinat sumbu y dari setiap swimlane
+                for act in tree.findall('.//Lanes'):
+                    for sub_child in act.findall('.//Coordinates'):
+                        Y_Coordinates = sub_child.get('YCoordinate')
+                        Y_Coordinates_cnvrt = int(Y_Coordinates)
+                        Lane_Coordinates.append(Y_Coordinates_cnvrt)
+
+                uniques=[]
+                dups=[]
+
+                Y_ordinate=[] #Y_Ordinate merupakan variable koordinate y dari setiap aktivitas/activity
+                for check in tree.findall('.//Activity'):
+                    for sub_ordinate in check.findall('.//Coordinates'):
+                        ordinate_y = sub_ordinate.get('YCoordinate')
+                        ordinate_y_cnvrt = int(ordinate_y)
+                        Y_ordinate.append(ordinate_y_cnvrt)
+
+               
+                for i in range (0,len(Y_ordinate)):
+                    for j in range (0,len(Lane_Coordinates)):
+                        
+                        if (Y_ordinate[i] < Lane_Coordinates[j]):
+                            print (Y_ordinate[i])
+
+                # for each in Coordinates:
+                #     if each not in uniques:
+                #         uniques.append(each)
+                #     else:
+                #         dups.append(each)
+                # print(dups)
+                # for i in range (0,len(Coordinates)):
+                #     for j in range (i+1,len(Coordinates)):
+                #         if (Coordinates[i] == Coordinates[j]):
+                #             final_coordinate.append(Coordinates[i])
+                
+                            
+                        # for check in sub_child:
+                        #     check = check.get('YCoordinate')
+                        #     if (Y_Coordinates == check):
+
+            else : 
+                print("less than 2")
+
+        context = {'list_actor':list_actor, 'list_activity':list_activity}
+    return render(request,'userstoriesresult.html',{'context': context, 'total_actor' : total_actor})
