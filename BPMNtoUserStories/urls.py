d3a8b4f5cfb4a1997604b48a245daa3c2aa9cc7c
Kinaraalg <Kinaraalghifari17@gmail.com>
2021-05-24 09:15:47 +0700

    Progress minggu kemarin

--- BPMNtoUserStories/urls.py
+++ BPMNtoUserStories/urls.py
@@ -10,6 +10,7 @@ urlpatterns = [
     path('', views.home, name='home'),
     path('uploadfile/', views.uploadpage, name='uploadpage'),
     path('proses/', views.parsing, name='parsing'),
+    path('about/', views.about, name='about'),
 ]
 if settings.DEBUG:
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
--- BPMNtoUserStories/views.py
+++ BPMNtoUserStories/views.py
@@ -10,6 +10,9 @@ def masterpages(request):
 def home(request):
      return render(request,'home.html')
      
+def about(request):
+     return render(request,'about.html')
+
 def uploadpage(request):
      return render(request,'uploadfile.html')
 
@@ -23,7 +26,7 @@ def parsing(request):
         full_path = os.path.join(dir,file_name)
         file_output = "clean" +file_name
         full_path_new = os.path.join(dir,file_output)
-        with open(full_path, 'r') as infile, open(full_path_new, "w") as outfile:
+        with open(full_path, 'r') as infile, open(full_path_new, "w") as outfile: #MEMBERSIHKAN FORMAT XPDL AGAR TAG PADA XML TERDEFINISI
             data = infile.read()
             data = data.replace('xmlns="http://www.wfmc.org/2009/XPDL2.2"', '')
             outfile.write(data)
@@ -34,15 +37,14 @@ def parsing(request):
             root = tree.getroot()
             list_actor = []
             list_activity = []
-            for i in tree.findall('.//Lane'):
+            for i in tree.findall('.//Lane'):       #PASSING UNTUK NILAI KOMPONEN USER STORIES
                 name = i.get('Name')
                 list_actor.append(name)
             for i in tree.findall('.//Activity'):
                 activity = i.get('Name')
-                if (activity != "Start" and activity != "End" and activity != None):
+                if (activity != "Start" and activity != "End" and "?" not in activity and activity != None):
                     list_activity.append(activity)
             total_actor = len(list_actor)
-            
             
 
             # mengecek apakah total aktor lebih dari satu dan mengambil nilai koordinat dari setiap lane didalam array
@@ -51,47 +53,62 @@ def parsing(request):
                 for act in tree.findall('.//Lanes'):
                     for sub_child in act.findall('.//Coordinates'):
                         Y_Coordinates = sub_child.get('YCoordinate')
-                        Y_Coordinates_cnvrt = int(Y_Coordinates)
+                        Y_Coordinates_cnvrt = float(Y_Coordinates)
                         Lane_Coordinates.append(Y_Coordinates_cnvrt)
-                uniques=[]
-                dups=[]
-
+                
                 Y_ordinate=[] #Y_Ordinate merupakan variable koordinate y dari setiap aktivitas/activity
                 for check in tree.findall('.//Activity'):
                     for sub_ordinate in check.findall('.//Coordinates'):
                         ordinate_y = sub_ordinate.get('YCoordinate')
-                        ordinate_y_cnvrt = int(ordinate_y)
+                        print(type(ordinate_y))
+                        ordinate_y_cnvrt = float(ordinate_y)
                         Y_ordinate.append(ordinate_y_cnvrt)
                 
-                last_actor=[]
-                actor_before_last=[]
-                for j in range (0,len(Lane_Coordinates)):
+                Array = []
+                ACT = []
+                for i in tree.findall('.//Lane'):  
+                    name = i.get('Name')
+                    ACT.append(name)
+
+                for i in range (0,len(ACT)):
+                    ACT[i] = []
+                    Array.append(ACT[i])
+
+                if (len(list_actor)>2):
+                    for j in range (1,(len(Lane_Coordinates)-1)):
+                        for i in range (0,len(Y_ordinate)):
+                            if (Y_ordinate[i] < Lane_Coordinates[len(Lane_Coordinates)-(len(Lane_Coordinates)-1)]): #Mendefinisikan Aktor Pertama dan Koordinat Y
+                                ACT[0].append(Y_ordinate[i])
+                                ACT[0] = list(set(ACT[0]))
+                            elif (Y_ordinate[i] > Lane_Coordinates[len(Lane_Coordinates)-1]):
+                                ACT[(len(Lane_Coordinates)-1)].append(Y_ordinate[i])
+                                ACT[(len(Lane_Coordinates)-1)] = list(set(ACT[(len(Lane_Coordinates)-1)]))
+                            elif (Y_ordinate[i]>Lane_Coordinates[j] and Y_ordinate[i]<Lane_Coordinates[j+1]):
+                                ACT[j].append(Y_ordinate[i])
+                                ACT[j] = list(set(ACT[j]))
+                else:
                     for i in range (0,len(Y_ordinate)):
-                        # if (Y_ordinate[i] > Lane_Coordinates[j] and Y_ordinate[i] < Lane_Coordinates[j+1]):
-                        #     actor_before_last.append(Y_ordinate[i])
-                        if (Y_ordinate[i] > Lane_Coordinates[len(Lane_Coordinates)-1]):
-                            last_actor.append(Y_ordinate[i])
+                        for j in range (0,len(Y_ordinate)):
+                            if (Y_ordinate[i] < Lane_Coordinates[len(Lane_Coordinates)-(len(Lane_Coordinates)-1)]): #Mendefinisikan Aktor Pertama dan Koordinat Y
+                                ACT[0].append(Y_ordinate[i])
+                                ACT[0] = list(set(ACT[0]))
+                            elif (Y_ordinate[i] > Lane_Coordinates[len(Lane_Coordinates)-1]):
+                                ACT[(len(Lane_Coordinates)-1)].append(Y_ordinate[i])
+                                ACT[(len(Lane_Coordinates)-1)] = list(set(ACT[(len(Lane_Coordinates)-1)]))
+                                    
+                for j in range (0,(len(ACT))):
+                    for i in range (0,(len(ACT[j]))):
+                        checking = ACT[j][i]
+                        for elem in tree.findall('.//Activity/NodeGraphicsInfos/NodeGraphicsInfo/Coordinates[@YCoordinate="%s"]......' %checking):
+                            activity_multiple = elem.get('Name')
+                            ACT[j].append(activity_multiple)
+                      
+                for i in tree.findall('.//Lane'):       #MENAMBAHKAN AKTOR KEDALAM ARRAY
+                    name = i.get('Name')
+                    ACT.append(name)
 
-                print("===LAST ACTOR ACTIVITY===")
-                print(last_actor)
-                # print("===ACTOR BEFORE LAST===")
-                # print(actor_before_last)
-
-                # for each in Coordinates:
-                #     if each not in uniques:
-                #         uniques.append(each)
-                #     else:
-                #         dups.append(each)
-                # print(dups)
-                # for i in range (0,len(Coordinates)):
-                #     for j in range (i+1,len(Coordinates)):
-                #         if (Coordinates[i] == Coordinates[j]):
-                #             final_coordinate.append(Coordinates[i])
-                
-                            
-                        # for check in sub_child:
-                        #     check = check.get('YCoordinate')
-                        #     if (Y_Coordinates == check):
+                # for i in range (0,len(ACT)):
+                #     print(ACT[i])
 
             else : 
                 print("less than 2")
