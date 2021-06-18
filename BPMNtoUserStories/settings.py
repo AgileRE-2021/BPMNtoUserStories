4f665c4ca8d93f05c72d8637d7e1785166b50b67
Dani Rahmatulloh <muhammad.dani.rahmatulloh-2018@fst.unair.ac.id>
2021-06-18 20:05:56 +0700

    update changes sqllite

--- BPMNtoUserStories/settings.py
+++ BPMNtoUserStories/settings.py
@@ -75,12 +75,8 @@ WSGI_APPLICATION = 'BPMNtoUserStories.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.mysql',
-        'NAME': 'bpmnus',
-        'USER': 'root',
-        'PASSWORD':'',
-        'HOST':'localhost',
-        'PORT':'3306',
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME'  : 'db.sqlite3',
     }
 }
 
