4fc95e8c69bac4c94cbc26e263e74a9977ead737
hisyamfariqi <hisyamfariqi@gmail.com>
2021-06-25 21:56:30 +0700

    update views

--- BPMNtoUserStories/urls.py
+++ BPMNtoUserStories/urls.py
@@ -1,6 +1,6 @@
 from django.contrib import admin
 from django.urls import path
-from . import views
+from bpmn_to_us import views
 from django.conf import settings
 from django.conf.urls.static import static
 
